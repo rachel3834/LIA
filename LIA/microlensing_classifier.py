d66681cb176e9d7c191b933e88cd8c37f92b2f6b
rachel3834 <rstreet@lco.global>
2019-11-22 14:36:01 -0800

    Set v1.1 for ANTARES

--- LIA/microlensing_classifier.py
+++ LIA/microlensing_classifier.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
     Created on Sat Jan 21 23:59:14 2017
-    
+
     @author: danielgodinez
 """
 import numpy as np
@@ -10,23 +10,25 @@ from sklearn import decomposition
 from warnings import warn
 from LIA import extract_features
 
+VERSION = 'antares_v1.1'
+
 def predict(mag, magerr, rf_model, pca_model):
     """This function uses machine learning to classify any given lightcurve as either
-        a cataclysmic variable (CV), a variable source, microlensing, or a constant star 
+        a cataclysmic variable (CV), a variable source, microlensing, or a constant star
         displaying no variability.
-        
+
     Parameters
     ----------
     mag : array
         Magnitude array.
     magerr : array
-        Corresponing photometric errors.  
+        Corresponing photometric errors.
     rf_model: fn
         The random forest model created using the function in the
         create_models module.
     pca_model: fn
         The PCA transformation devised using the function in the
-        create_models module. 
+        create_models module.
 
     Returns
     -------
@@ -49,11 +51,10 @@ def predict(mag, magerr, rf_model, pca_model):
     stat_array = array.append(extract_features.extract_all(mag, magerr, convert=True))
     array=np.array([i for i in array])
     stat_array = pca_model.transform(array)
-    
+
     #prediction =rf_model.predict(stat_array)
     pred = rf_model.predict_proba(stat_array)
     cons_pred, cv_pred, ml_pred, var_pred = pred[:,0],pred[:,1],pred[:,2],pred[:,3]
     prediction = classes[np.argmax(pred)]
-    
-    return prediction, ml_pred, cons_pred, cv_pred, var_pred
 
+    return prediction, ml_pred, cons_pred, cv_pred, var_pred
